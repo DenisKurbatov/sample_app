e710296e8ad256c80c3017cace66da2119d9f40b
Denis Kurbatov <kurbatov.work@gmail.com>
2018-03-28 06:52:20 +0300

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    #Проверяет, что после отправки формы, мы вернулись на страницу users/new
+    assert_template 'users/new'
+  end
+end
